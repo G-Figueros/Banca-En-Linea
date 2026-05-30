2be04654ec7c07a05c4b41b5c1fdd3af98208ab0
G-Figueros <57198941+G-Figueros@users.noreply.github.com>
2026-05-29 23:21:17 -0600

    correccion funcionamiento

--- BancaEnLinea/src/services/account.service.js
+++ BancaEnLinea/src/services/account.service.js
@@ -1,4 +1,4 @@
-import { apiGet, apiPost, apiPut } from './http.service'
+import { apiGet, apiPost, apiPatch } from './http.service'
 
 export const getMyAccounts = () => {
   return apiGet('/allcuentas')
@@ -12,14 +12,14 @@ export const getAccountById = (id) => {
   return apiGet(`/api/Cuentas/cuenta/${id}`)
 }
 
-export const getAccountMovements = (id) => {
-  return apiGet('/alltransferencias')
+export const getAccountMovements = () => {
+  return apiGet('/allmovimientoscuenta')
 }
 
 export const createAccount = (account) => {
   return apiPost('/createcuenta', account)
 }
 
-export const updateAccountStatus = (id) => {
-  return apiPut(`/togglecuenta/${id}`)
+export const updateAccountStatus = (id, status) => {
+  return apiPatch(`/cuenta/${id}/status`, { status })
 }
